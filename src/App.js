a1d0e405d7ae78dd968c19c1aecce655e5ad3d01
cathykwc <71102690+cathykwc@users.noreply.github.com>
2020-12-02 21:06:22 -0800

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>This project is coded by{" "}<a href="https://ca.fullscript.com/welcome/cchan" target="_blank">Cathy Chan</a>  and is{" "}<a href="https://github.com/cathykwc/react-weather-app" target="_blank">open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
