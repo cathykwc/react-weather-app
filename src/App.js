fd80bf1df024bdb169ce5caccc8d0a4003b7f3ea
cathykwc <71102690+cathykwc@users.noreply.github.com>
2020-12-02 22:22:42 -0800

    edited target link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer>This project is coded by{" "}<a href="https://ca.fullscript.com/welcome/cchan" target="_blank">Cathy Chan</a>  and is{" "}<a href="https://github.com/cathykwc/react-weather-app" target="_blank">open-sourced on Github</a>
+      <footer>This project is coded by{" "}<a href="https://ca.fullscript.com/welcome/cchan" target="blank">Cathy Chan</a>  and is{" "}<a href="https://github.com/cathykwc/react-weather-app" target="blank">open-sourced on Github</a>
       </footer>
     </div>
   );
