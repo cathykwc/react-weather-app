26e817177e3412544cd1c1ec7a4c259408022018
cathykwc <71102690+cathykwc@users.noreply.github.com>
2020-12-03 11:53:37 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>This project is coded by{" "}<a href="https://ca.fullscript.com/welcome/cchan" target="blank">Cathy Chan</a>  and is{" "}<a href="https://github.com/cathykwc/react-weather-app" target="blank">open-sourced on Github</a>
       </footer>
+    </div>
     </div>
   );
 }
