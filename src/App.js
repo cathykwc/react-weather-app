b2c581ae4b876be8440a71e1352e2d4e25104d7d
cathykwc <71102690+cathykwc@users.noreply.github.com>
2021-03-11 11:34:28 -0800

    updated footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <Weather defaultLocation="Barcelona"/>
-      <footer>This project is coded by{" "}<a href="https://ca.fullscript.com/welcome/cchan" target="blank">Cathy Chan</a>  and is{" "}<a href="https://github.com/cathykwc/react-weather-app" target="blank">open-sourced on Github</a>
+      <footer>This project was coded by{" "}<a href="https://ca.fullscript.com/welcome/cchan" target="blank">Cathy Chan</a>  and is{" "}<a href="https://github.com/cathykwc/react-weather-app" target="blank">open-sourced on Github</a>
       </footer>
     </div>
     </div>
